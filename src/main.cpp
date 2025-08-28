d318b72c3b486f0fcd9b2f71206551b1a4fcf085
HKU Aaron Account <Aaron@YA08.hku.local>
2025-08-28 14:52:41 +0200

    Starting repo

---
+++ src/main.cpp
@@ -0,0 +1,43 @@
+#include "imgui.h"
+#include "imgui-SFML.h"
+
+#include <SFML/Graphics.hpp>
+
+int main() {
+    sf::RenderWindow window;
+    window.create(sf::VideoMode({ 1280, 720 }), "My window");
+    window.setFramerateLimit(60);
+    window.setVerticalSyncEnabled(true);
+
+    if (!ImGui::SFML::Init(window))
+        return -1;
+
+    sf::Clock deltaClock;
+    while (window.isOpen())
+    {
+        float deltaTime = deltaClock.restart().asSeconds();
+
+        // Event Polling
+        while (const std::optional event = window.pollEvent())
+        {
+            ImGui::SFML::ProcessEvent(window, *event);
+
+            // "close requested" event: we close the window
+            if (event->is<sf::Event::Closed>())
+                window.close();
+        }
+
+        // Update
+        ImGui::SFML::Update(window, deltaClock.restart());
+        // ImGui::ShowDemoWindow();
+
+        // Render
+        window.clear();
+
+        ImGui::SFML::Render(window);
+
+        window.display();
+    }
+
+	return 0;
+}
