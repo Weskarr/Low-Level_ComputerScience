d658fb986aa7bedb903f9c951889096b157d8100
Weskarr <141842001+Weskarr@users.noreply.github.com>
2025-09-05 11:39:52 +0200

    Added Comments in Main & Templates.

--- Assignments/0_StartingPoint/SourceFiles/main.cpp
+++ Assignments/0_StartingPoint/SourceFiles/main.cpp
@@ -2,42 +2,48 @@
 #include "imgui-SFML.h"
 #include <SFML/Graphics.hpp>
 
-#include <iostream> // For Debugging
+#include <iostream> // For Debugging.
 
 int main()
 {
-    std::cout << "Initializing: Main (StartingPoint)" << std::endl;
+    // Console Initialization.
+    std::cout << "Initializing: Main (0_StartingPoint)" << std::endl;
 
+    // Create SFML window.
     sf::RenderWindow window;
     window.create(sf::VideoMode({ 1280, 720 }), "My window");
     window.setFramerateLimit(60);
     window.setVerticalSyncEnabled(true);
 
+    // Init check for ImGui.
     if (!ImGui::SFML::Init(window))
         return -1;
 
+    // Clock for ImGui updates.
     sf::Clock deltaClock;
+
+    // Main Loop.
     while (window.isOpen())
     {
-        // Event Polling
+        // Event Polling.
         while (const std::optional event = window.pollEvent())
         {
             ImGui::SFML::ProcessEvent(window, *event);
 
-            // "close requested" event: we close the window
+            // "close requested" event: we close the window.
             if (event->is<sf::Event::Closed>())
                 window.close();
         }
 
-        // Update
+        // Update ImGui state.
         ImGui::SFML::Update(window, deltaClock.restart());
+
+        // Show ImGui Demo.
         //ImGui::ShowDemoWindow();
 
-        // Render
+        // Render everything.
         window.clear();
-
         ImGui::SFML::Render(window);
-
         window.display();
     }
 
